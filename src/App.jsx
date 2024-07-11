6f3de97948280dbdf30707bd6e183750fe3a1399
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-07-11 16:11:12 +0300

    fixed some issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import JobPosting from './pages/JobPosting'
-import { Login } from "./components/Login"
-import { Register } from "./components/Register"
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-    <JobPosting/>
-    <Login/>
-    <Register/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Corrected how the component is rendered */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
