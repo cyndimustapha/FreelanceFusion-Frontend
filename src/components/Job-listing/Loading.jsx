1ff025bdd0ea5ba112558e465dcd2c355379ab0b
Mwaijoe <brakejoe9@gmail.com>
2024-07-11 10:45:20 +0300

    merge

---
+++ src/components/Job-listing/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <div className="loading-text">Loading...</div>
+    </div>
+  );
+};
+
+export default Loading;
