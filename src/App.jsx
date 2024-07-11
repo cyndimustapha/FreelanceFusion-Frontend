4111adb5304df727b8135b7cd4cf35c19d79e9ab
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-07-11 12:20:53 +0300

    edit the css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import JobPosting from './pages/JobPosting'
+import { Login } from "./components/Login"
+import { Register } from "./components/Register"
+
 
 function App() {
   return (
     <>
     <JobPosting/>
+    <Login/>
+    <Register/>
     </>
   )
 }
