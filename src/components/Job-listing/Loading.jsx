b9914b3396e34d37250e9f8064916d64b9640b88
Mwaijoe <brakejoe9@gmail.com>
2024-07-11 09:21:37 +0300

    change

--- src/components/Job-listing/Loading.jsx
+++ src/components/Job-listing/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className="loading-container">
       <div className="spinner"></div>
-      <div className="loading-text">Loading...</div>
+      <div className="loading-text">Loading..</div>
     </div>
   );
 };
