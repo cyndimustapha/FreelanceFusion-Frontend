d1ea33051360c86b35b4d3f02a32c68b725b559f
kenkipkoec <itsmetrokenaki@gmal.com>
2024-07-12 20:59:24 +0300

    done with homepage and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import DashBoard from './components/DashBoard';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import JobListings from './components/JobListings';
 import JobDetails from './components/JobDetails';
 import PostJob from './components/PostJob';
+import Bids from './components/Bids';
 import Profile from './components/Profile';
 
-function App() {
+const App = () => {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
-        <ProtectedRoute path="/dashboard" component={DashBoard} />
-        <ProtectedRoute path="/jobs" exact component={JobListings} />
-        <ProtectedRoute path="/jobs/:id" component={JobDetails} />
-        <ProtectedRoute path="/post-job" component={PostJob} />
-        <ProtectedRoute path="/profile" component={Profile} />
+        <PrivateRoute path="/dashboard" component={Dashboard} isAuthenticated={isAuthenticated} />
+        <PrivateRoute path="/job-listings" component={JobListings} isAuthenticated={isAuthenticated} />
+        <PrivateRoute path="/job-details/:id" component={JobDetails} isAuthenticated={isAuthenticated} />
+        <PrivateRoute path="/post-job" component={PostJob} isAuthenticated={isAuthenticated} />
+        <PrivateRoute path="/bids" component={Bids} isAuthenticated={isAuthenticated} />
+        <PrivateRoute path="/profile" component={Profile} isAuthenticated={isAuthenticated} />
+        <Redirect to="/" />
       </Switch>
     </Router>
   );
-}
+};
+
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
 
 export default App;
