17507f1a137c019cb69074f25b6f39c7ee7a7655
Mwaijoe <brakejoe9@gmail.com>
2024-07-12 22:40:45 +0300

    Fix Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import JobPosting from './pages/JobPosting'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import JobPosting from './pages/JobPosting';
+import JobListings from './components/Job-listing/JobListings';
+import JobDetails from './components/Job-listing/JobDetails';
 
 function App() {
   return (
-    <>
-    <JobPosting/>
-    </>
-  )
+    <Routes>
+      <Route path="/jobs" element={<JobListings />} />
+      <Route path="/jobs/:id" element={<JobDetails />} />
+      <Route path="/jobs/posting" element={<JobPosting />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
