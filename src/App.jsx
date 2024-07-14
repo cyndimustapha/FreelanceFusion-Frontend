87faaa49eda4069368a3f78febd8eac6d02d6562
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-07-14 11:56:46 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import JobPosting from './pages/JobPosting'
-
+import Home from './pages/Home'
 function App() {
   return (
     <>
+    <Home/>
     <JobPosting/>
     </>
   )
