cc6a63eb3d190bc38467b77623b8671a0c155550
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-07-14 11:40:48 +0300

    resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import JobPosting from './pages/JobPosting'
-import { Login } from "./components/Login"
-import { Register } from "./components/Register"
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './components/SignIn';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <>
-    <JobPosting/>
-    <Login/>
-    <Register/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
