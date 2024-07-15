4dd370e0b93bfdd89ccae5f407275d2869011f19
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-07-15 12:44:52 +0300

    authentication

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import "./App.css";
 
 import ChannellListContainer from "./components/ChannellListContainer";
 import ChannelContainer from "./components/ChannelContainer";
+import Auth from "./components/Auth";
+
 const apiKey = `nf7gevqn84xk`;
 const client = StreamChat.getInstance(apiKey);
+const authToken = false;
 
 function App() {
+  if (!authToken) return <Auth />;
   return (
     <div className="app__wrapper">
       <Chat client={client}>
