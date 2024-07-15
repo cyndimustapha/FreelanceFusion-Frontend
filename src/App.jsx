a75e654c89ae8760888471c093ae6f0c12bb151b
kenkipkoec <itsmetrokenaki@gmal.com>
2024-07-15 23:09:32 +0300

    started afresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Navbar from './pages/Navbar';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import JobListings from './pages/JobListings';
-import JobDetails from './pages/JobDetails';
-import PostJob from './pages/PostJob';
-import Profile from './pages/Profile';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./pages/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import JobListings from "./pages/JobListings";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <ProtectedRoute path="/dashboard" component={Dashboard} />
-        <ProtectedRoute path="/jobs" exact component={JobListings} />
-        <ProtectedRoute path="/jobs/:id" component={JobDetails} />
-        <ProtectedRoute path="/post-job" component={PostJob} />
-        <ProtectedRoute path="/profile" component={Profile} />
-      </Switch>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/job-listings" element={<JobListings />} />
+      </Routes>
     </Router>
   );
 }
-
 
 export default App;
